61ccfd4d1095a7d6aede4752a83e22f8adb43ef0
Chris White <cxwembedded@gmail.com>
2020-12-19 13:14:37 -0500

    Perl: add license and copyright statement
    - Add LICENSE file
    - Add SPDX identifier
    
    Closes #1.  Reference:
    <https://github.com/Habbie/dpkg-version-sort/issues/1#issuecomment-748510447>

--- dpkg-version-sort.pl
+++ dpkg-version-sort.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 use strict;
 use warnings;
 use sort 'stable';
@@ -26,3 +28,50 @@ my @l = <>;
 chomp(@l);
 my @s = sort $sorter @l;
 print join("\n", @s), "\n";
+
+__END__
+
+=head1 NAME
+
+dpkg-version-sort.pl - Sort Debian package versions
+
+=head1 USAGE
+
+  <whatever> | dpkg-version-sort.pl
+
+That's it!
+
+=head1 AUTHORS
+
+=over
+
+=item *
+
+Peter van Dijk C<< <peter@7bits.nl> >>
+
+=item *
+
+Christopher White C<< <cxwembedded@gmail.com> >>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (c) 2015 Peter van Dijk.
+
+Copyright (c) 2020 Christopher White.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+=cut
