42951c4f7b20e9ab7dd25959b9cb5bc4acaac1bf
Chris White <cxwembedded@gmail.com>
2020-12-19 13:13:04 -0500

    Perl: use Dpkg::Version for faster sort if it's available
    If module Dpkg::Version is installed, use it instead of shelling out
    to `dpkg --compare-versions` for each comparison.
    
    Also, make dpkg-version-sort.pl executable.

--- dpkg-version-sort.pl
+++ dpkg-version-sort.pl
@@ -1,11 +1,28 @@
 #!/usr/bin/perl
-sub dpkgcomp
+use strict;
+use warnings;
+use sort 'stable';
+
+# Fast sorter, if it is installed and not overridden by $DPKG_VERSION_SORT_FAST.
+# This module is part of libdpkg-perl on Ubuntu.
+my $have_fast = eval "use Dpkg::Version; 1";
+my $fast = $have_fast;
+if(exists $ENV{DPKG_VERSION_SORT_FAST}) {
+  $fast = $have_fast && !!$ENV{DPKG_VERSION_SORT_FAST};
+}
+
+# Slow sorter
+sub dpkgcomp($$)
 {
+  my ($a, $b) = @_;
   return 1 if(system("dpkg --compare-versions $a lt $b"));
   return -1 if(system("dpkg --compare-versions $a gt $b"));
   return 0;
 }
-@l = <>;
+
+my $sorter = $fast ? \&Dpkg::Version::version_compare : \&dpkgcomp;
+
+my @l = <>;
 chomp(@l);
-@s = sort dpkgcomp @l;
+my @s = sort $sorter @l;
 print join("\n", @s), "\n";
