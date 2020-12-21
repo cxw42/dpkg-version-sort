da45a859cec31a946228165cb886e6e863ec7bda
Chris White <cxwembedded@gmail.com>
2020-12-20 20:58:34 -0500

    Use two-space tabs for Python as well as Perl
    Per
    <https://github.com/Habbie/dpkg-version-sort/pull/2#discussion_r546299675>

--- dpkg-version-sort.py
+++ dpkg-version-sort.py
@@ -2,9 +2,9 @@
 import os, sys
 
 def compare(a,b):
-	if os.system("dpkg --compare-versions %s lt %s" % (a,b)): return 1 
-	if os.system("dpkg --compare-versions %s gt %s" % (a,b)): return -1
-	return 0;
+  if os.system("dpkg --compare-versions %s lt %s" % (a,b)): return 1
+  if os.system("dpkg --compare-versions %s gt %s" % (a,b)): return -1
+  return 0;
 
 versions = [s.strip() for s in sys.stdin]
 versions.sort(cmp=compare)
